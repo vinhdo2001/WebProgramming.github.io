b4c0026bc858fc24c6a843129a878c2e42328993
NguyenDangLamPhuong <s3754105@rmit.edu.vn>
2021-05-08 10:49:44 +0700

    "my account" to login/ redirect to my profile"

--- checkVisitedLink.js
+++ checkVisitedLink.js
@@ -1,17 +1,27 @@
 //nnếu visit lần đầu auto qua trang login
 // nếu lần 2 thì có 2 cases:  qua trang login nếu chưa login, hoặc qua  my account nếu login rồi
 
-const addButton = document.querySelector('#activeLink');
+var addButton = document.querySelector('#activeLink');
+
 addButton.addEventListener("click", () => {
+    //lần đầu visit link
     if (localStorage.getItem('visited') == null) {
-        document.getElementsByClassName('activeLink').src = 'Login.html';//qua login page phải login thành công thì visited =true và alreadyLogin =true
+        document.getElementsByClassName('activeLink').href = 'Login.html';                                    //qua login page phải login thành công thì visited =true và alreadyLogin =true
+        localStorage.setItem('visited', JSON.stringify(true))
+        // window.location.href
+        // localStorage.setItem('alreadyLogin', JSON.stringify(true))
     }
+    //những lần sau visit link
     else {
+        //chưa login
         if (localStorage.getItem('alreadyLogin') == null) {
-            document.getElementsByClassName('activeLink').src = 'Login.html';//qua login page phải login thành công thì visited =true và alreadyLogin =true
+            document.getElementsByClassName('activeLink').href = 'Login.html';                            //qua login page phải login thành công thì visited =true và alreadyLogin =true
+            localStorage.setItem('alreadyLogin', JSON.stringify(true))
+            localStorage.setItem('visited', JSON.stringify(true))
         }
+        //login ròi
         else {
-            document.getElementsByClassName('activeLink').src = 'MyAccount(logged-in).html';
+            document.getElementsByClassName('activeLink').href = 'MyAccount(logged-in).html';
         }
     }
 });
