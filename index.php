9af9e2bdbd11600936af183130e95b55d7269f7e
s3789355 <56541632+s3789355@users.noreply.github.com>
2021-06-01 23:59:05 +0700

    Revert "part 07"
    This reverts commit 19a905d5c23243f0ef740586caea7ce8dea83cf5.

--- index.php
+++ index.php
@@ -59,12 +59,7 @@
                             <a href="Contact.html">Contact</a>
                         </li>
                         <li>
-                        <?php
-                            echo"
-                            <a href='/storeHome.php'> Browse</a>";
-                            
-                        ?>
-                         
+                            <a href="product.php">Product Browser</a>
 
                         </li>
                     </ul>
@@ -99,11 +94,10 @@
             // $new_store_names=array();
             $new_store_count=0;
             foreach($new_stores as $new_store){
-                $new_stores_id=$new_store['id'];
                 $new_stores_name=$new_store['name'];
                 echo"
                     <div class='slider-item1'>
-                        <a href='/storeHome.php?id={$new_stores_id}&name={$new_stores_name}'>
+                        <a href=''>
                             <div class='img-div1'></div>
                             <h4>$new_stores_name</h4>
                             <div class='rating'>
@@ -139,7 +133,7 @@
                 $price_new_products=$new_product['price'];
                     echo"
                     <div class='slider-item1'>
-                        <a href='/ProductDetail.php?name={$new_products_name}&price={$price_new_products}'>
+                        <a href='/ProductDetail2.php?name={$new_products_name}&price={$price_new_products}'>
                             <div class='img-div'></div>
                             <h4>$new_products_name </h4>
                             <div class='rating'>
@@ -185,11 +179,10 @@
           ];
           $featured_stores_count = 0;
       foreach($featured_stores as $featured_store){
-        $featured_store_id = $featured_store['id'];
         $featured_store_name = $featured_store['name'];
         
         echo"
-        <a href='/storeHome.php?id={$featured_store_id}&name={$featured_store_name}'>
+        <a href='hnoss.html'>
             <div class='.col-3'>
                 <img src='$featured_stores_images[$featured_stores_count]' alt=''>
             </div>
@@ -231,7 +224,7 @@
         $name_featured_product = $featured_product['name'];
         $price_featured_product = $featured_product['price'];
         echo"
-        <a href='/ProductDetail.php?name={$name_featured_product}&price={$price_featured_product}'>
+        <a href='/ProductDetail2.php?name={$name_featured_product}&price={$price_featured_product}'>
             <div class='.col-3'>
                 <img src='$featured_products_images[$featured_products_count]'
                     alt=''>
