b4cf4215a624056b70febf78a8e9a4969d3db2a3
s3789355 <56541632+s3789355@users.noreply.github.com>
2021-06-01 18:58:39 +0700

    c

---
+++ product.js
@@ -0,0 +1,77 @@
+var button = document.getElementById("add-button")
+var buyButton = document.getElementById("buy-button")
+var products = ["The Speed", "The Strong"]
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+if(span){
+	span.onclick = function() {
+		modal.style.display = "none";
+	  }
+	window.onclick = function(event) {
+		if (event.target == modal) {
+		  modal.style.display = "none";
+		}
+	  }
+}
+
+cartNumber()
+if(button){
+	button.addEventListener('click',() => {    
+    cartUpdate()
+    popUp()
+})}
+if(buyButton){
+	buyButton.addEventListener('click',() => {    
+    cartUpdate()
+})}
+
+
+function cartNumber(){
+	var num = 0
+	for (var i in localStorage) {
+        if (localStorage.getItem(i) != null && products.includes(i)){
+			string = localStorage.getItem(i)
+            num += parseInt(string.split(",")[1])
+		}
+	}
+	document.getElementsByClassName('cart-number')[0].innerHTML = `${num}<img src = "img/cart.png" class = "cart-icon">`
+	var icon = document.getElementsByClassName("cart-icon")[0]
+	if(num < 10){
+		icon.style.right = "29px";
+	}
+	if(num >= 10) {
+		icon.style.right = "35px";
+	}
+
+	
+}
+function cartUpdate() {
+    var n = false;
+    if(localStorage.getItem(document.getElementById("product-name").innerText) != null){
+        var string = localStorage.getItem(document.getElementById("product-name").innerText)
+        n = parseInt(string.split(",")[1])
+    }
+    if(n){
+        localStorage.setItem(document.getElementById("product-name").innerText, [parseInt(document.getElementById("product-price").innerText.replace("$","")),n+1])
+    }
+    else{
+        localStorage.setItem(document.getElementById("product-name").innerText, [parseInt(document.getElementById("product-price").innerText.replace("$","")),1])
+    }
+	cartNumber()
+
+}
+
+function popUp() {
+    var popup = document.getElementById("card");
+    popup.style.visibility ="visible"
+    popup.classList.add("show");
+    setTimeout(function(popup){ 
+        var popup = document.getElementById("card");
+        popup.style.visibility ="hidden"; 
+        popup.classList.remove("show");
+        }, 200);
+
+
+	}
---
+++ product.php
@@ -0,0 +1,208 @@
+<?php
+session_start();
+require "productprocessing.php";
+$all_products = getAllProducts($_SESSION["storeID"]);
+$number_of_products = count($all_products);
+$limit = 2;
+$page = (isset($_GET['page']) && is_numeric($_GET['page']) ) ? $_GET['page'] : 1;
+$paginationStart = ($page - 1) * $limit;
+$no_of_lines = $number_of_products;
+$new_arrivals = ["", "", "", "",""];
+$allRecrods = $no_of_lines;
+// Calculate total pages
+$totalPages = ceil($allRecrods / $limit);
+// Prev + Next
+$prev = $page - 1;
+$next = $page + 1;
+$path = "products.csv";
+$last_product = $paginationStart + $limit;
+$sort_value = 1;
+if(isset($_GET['sort'])){
+    $sort_products = sortSelect($_GET['sort']);
+}
+else{
+    $sort_products = sortCSVFile($path);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simba | All Products</title>
+    <link rel="stylesheet" href="Home.css">
+    <link rel="stylesheet" href="NavBar.css">
+    
+    <link href="Vinh's/cookie.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&family=Montserrat:wght@100&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                <?php
+                    echo "
+                    <a href='/index.php'>
+                        <img src='https://i.imgur.com/WnDfSUO.png' alt='Logo' width='65%'>
+                    </a>";
+                    
+                    ?>
+                </div>
+                <nav>
+                    <input type="checkbox" id="menu-icon">
+                    <label for="menu-icon">Menu</label>
+                    <ul id="MenuItems">
+                        <li>
+                            <a href="order-placement.html">Cart</a>
+                        <li>
+                        <li>
+                            <a href="AboutUs.html">About us</a>
+                        </li>
+                        <li>
+                            <a href="MyAccount(logged-in).html">Account</a>
+                        </li>
+                        <li>
+                            <a href="Fees.html">Fees</a>
+                        </li>
+
+                        <li>
+                            <a href="FAQs.html">FAQs</a>
+                        </li>
+                        <li>
+                            <a href="Contact.html">Contact</a>
+                        </li>
+                        <li>
+                            <a href="product.php">Product Browser</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- featured productes -->
+    <div class="small-container">
+        <!-- <h2 class="title">Featured Products</h2> -->
+        <div class="row row-2">
+            <h2 id="all-product">All Products</h2>
+            <select onchange="sort_products()" id="sort_select">
+                    <option value="1" <?php echo ($sort_value == 1) ? "selected" : ""; ?>>Sort by default</option>
+                    <option value="2" <?php echo ($sort_value == 2) ? "selected" : ""; ?>>Sort A-Z</option>
+                    <option value="3" <?php echo ($sort_value == 3) ? "selected" : ""; ?>>Sort by Price</option>
+                    <option value="4" <?php echo ($sort_value == 4) ? "selected" : ""; ?>>Sort by time (newest)</option>
+                    <option value="5" <?php echo ($sort_value == 5) ? "selected" : ""; ?>>Sort by time (oldest)</option>
+                </select>
+        </div>
+
+        <div class="row">
+            <?php displayProduct($sort_products,  $paginationStart, $last_product)?>
+        </div>
+        <div class="page-btn">
+            <?php 
+            if($page == 1){ 
+        ?>
+            <span class="active_pagination"><i class="fa fa-arrow-left"></i></span>
+            <?php
+            } else{
+        ?>
+            <span class="pagination_span"
+                onclick="window.location.href='<?php echo "product.php?sort=".$sort_value."&page=". $prev; ?>'"><i
+                    class="fa fa-arrow-left"></i></span>
+            <?php
+            }
+        ?>
+            <?php for($i = 1; $i <= $totalPages; $i++ ): 
+
+            if($page == $i){
+                ?>
+            <span class="active_pagination" onclick="window.location.href='#'"><?php echo $i; ?></span>
+            <?php
+            } else{
+                ?>
+            <span class="pagination_span"
+                onclick="window.location.href='product.php?sort=<?php echo $sort_value ?>&page=<?php echo $i; ?>'"><?php echo $i; ?></span>
+            <?php
+            }
+        ?>
+            <?php endfor; ?>
+            <?php 
+            if($page >= $totalPages){ 
+        ?>
+            <span class="active_pagination"><i class="fa fa-arrow-right"></i></span>
+            <?php
+            } else{
+        ?>
+            <span class="pagination_span"
+                onclick="window.location.href='<?php echo "product.php?sort=".$sort_value."&page=". $next; ?>'"><i
+                    class="fa fa-arrow-right"></i></span>
+            <?php
+            }
+        ?>
+        </div>
+       
+        </div>
+        <br><br>
+
+       
+
+
+    </div>
+    <div class="cookie-container">
+        <h2>I use cookies</h2>
+        <p>
+            My website uses cookies necessary for its basic functioning.
+            By continuing browsing, you consent to my use of cookies
+            and other technologies.
+        </p>
+        <button class="cookie-btn">
+            I Understand 
+        </button>
+        <a href="PrivacyPolicy.html"> Learn more</a>
+    </div>
+    <script src="Vinh's/cookie.js"></script>
+    <!-------footer---->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="footer-col-1">
+                    <img src="productImages/Logo.png" alt="">
+                </div>
+                <div class="footer-col-2">
+                    <a href="ToS.html">
+                        <p>ToS (Term of Service)</p>
+                    </a>
+
+                </div>
+                <div class="footer-col-3">
+                    <a href="PrivacyPolicy.html">
+                        <p>Privacy Policy</p>
+                    </a>
+
+                </div>
+
+                <div class="footer-col-4">
+                    <a href="CopyRight.html">
+                        <p>Copy right 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="main.js"></script>
+        <script src="product.js"></script>
+</body>
+
+</html>
