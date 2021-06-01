01b8d807ea92be3b6d756d8551ee5a3a68a69f2c
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2021-06-01 22:12:14 +0700

    Create ForgotPassword.php

---
+++ ForgotPassword.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
+    <title>Simba | Forgot Password</title>
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="Home.css">
+    <link rel="stylesheet" href="NavBar.css">
+    <link href="Vinh's/cookie.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&family=Montserrat:wght@100&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="index.html">
+                        <img src="https://i.imgur.com/WnDfSUO.png" alt="Logo" width="65%">
+                    </a>
+                </div>
+                <nav>
+                    <input type="checkbox" id="menu-icon">
+                    <label for="menu-icon">Menu</label>
+                    <ul id="MenuItems">
+                        <li>
+                            <a href="AboutUs.html">About us</a>
+                        </li>
+                        <li>
+                            <a href="MyAccount(logged-in).php">Account</a>
+                        </li>
+                        <li>
+                            <a href="Fees.html">Fees</a>
+                        </li>
+
+                        <li>
+                            <a href="FAQs.html">FAQs</a>
+                        </li>
+                        <li>
+                            <a href="Contact.html">Contact</a>
+                        </li>
+                        <li>
+                            <a href="ProductBrowser.html">Product Browser</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div class="account-retrieve">
+        <div class="ForgotPassword">
+            <div class="row">
+                <div class="col-2"> <img src="Image/ecommerce_Image.png" width="50%">
+                </div>
+            <div class="col-2">
+                <div class="findyouraccount">
+                    <h2> Find Your Account</h2> <br> 
+                    <h3> Enter your phone number or email to retrieve your account</h3> <br> 
+                    <input type="text" name="email" placeholder="Email or phone number"> <br>
+                    <button type="submit" class="button">Send</button><br>
+                </div>  
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="cookie-container">
+        <h2>I use cookies</h2>
+        <p>
+            My website uses cookies necessary for its basic functioning.
+            By continuing browsing, you consent to my use of cookies
+            and other technologies.
+        </p>
+        <button class="cookie-btn">
+            I Understand 
+        </button>
+        <a href="PrivacyPolicy.html"> Learn more</a>
+    </div>
+    <script src="Vinh's/cookie.js"></script>
+
+    <!-------footer---->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="footer-col-1">
+                    <img src="productImages/Logo.png" alt="">
+                </div>
+                <div class="footer-col-2">
+                    <a href="ToS.html">
+                        <p>ToS (Term of Service)</p>
+                    </a>
+
+                </div>
+                <div class="footer-col-3">
+                    <a href="PrivacyPolicy.html">
+                        <p>Privacy Policy</p>
+                    </a>
+
+                </div>
+
+                <div class="footer-col-4">
+                    <a href="CopyRight.html">
+                        <p>Copy right 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html> 
