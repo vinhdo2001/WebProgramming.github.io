b96940375f65574759269bf9270b5afec6757ae4
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2021-06-01 22:50:54 +0700

    Update CopyRight.php

--- CopyRight.php
+++ CopyRight.php
@@ -1,3 +1,5 @@
+<?php include 'deleteInstall.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,26 +31,26 @@
                     <label for="menu-icon">Menu</label>
                     <ul id="MenuItems">
                         <li>
-                            <a href="order-placement.html">Cart</a>
-                            <li>
+                            <a href="order-placement.php">Cart</a>
                         <li>
-                            <a href="AboutUs.html">About us</a>
+                        <li>
+                            <a href="AboutUs.php">About us</a>
                         </li>
                         <li>
                             <a href="MyAccount(logged-in).php">Account</a>
                         </li>
                         <li>
-                            <a href="Fees.html">Fees</a>
+                            <a href="Fees.php">Fees</a>
                         </li>
 
                         <li>
-                            <a href="FAQs.html">FAQs</a>
+                            <a href="FAQs.php">FAQs</a>
                         </li>
                         <li>
-                            <a href="Contact.html">Contact</a>
+                            <a href="Contact.php">Contact</a>
                         </li>
                         <li>
-                            <a href="ProductBrowser.html">Product Browser</a>
+                            <a href="ProductBrowser.php">Product Browser</a>
                         </li>
                     </ul>
                 </nav>
