d10081dea773151256a9842b23b670d7d3444391
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2021-06-01 22:51:37 +0700

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,110 @@
+<?php include 'deleteInstall.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simba | Online Shopping Mall</title>
+    <link rel="stylesheet" href="Home.css">
+    <link rel="stylesheet" href="NavBar.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="store.css">
+    <link href="Vinh's/cookie.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&family=Montserrat:wght@100&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="index.php">
+                        <img src="https://i.imgur.com/WnDfSUO.png" alt="Logo" width="65%">
+                    </a>
+                </div>
+                <nav>
+                    <input type="checkbox" id="menu-icon">
+                    <label for="menu-icon">Menu</label>
+                    <ul id="MenuItems">
+                        <li>
+                            <a href="order-placement.php">Cart</a>
+                        <li>
+                        <li>
+                            <a href="AboutUs.php">About us</a>
+                        </li>
+                        <li>
+                            <a href="MyAccount(logged-in).php">Account</a>
+                        </li>
+                        <li>
+                            <a href="Fees.php">Fees</a>
+                        </li>
+
+                        <li>
+                            <a href="FAQs.php">FAQs</a>
+                        </li>
+                        <li>
+                            <a href="Contact.php">Contact</a>
+                        </li>
+                        <li>
+                            <a href="ProductBrowser.php">Product Browser</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+    </div>
+
+
+    <img style="width: 100%;" class="center-fit" src='https://i.imgur.com/cBOSW8n.png'>
+    <!-- https://www.facebook.com/tontonfriends/photos/ive-always-wanted-to-tell-you/2234792799879551/ -->
+
+    <div class="cookie-container">
+        <h2>I use cookies</h2>
+        <p>
+            My website uses cookies necessary for its basic functioning.
+            By continuing browsing, you consent to my use of cookies
+            and other technologies.
+        </p>
+        <button class="cookie-btn">
+            I Understand 
+        </button>
+        <a href="PrivacyPolicy.php"> Learn more</a>
+    </div>
+    <script src="Vinh's/cookie.js"></script>
+    
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="footer-col-1">
+                    <img src="productImages/Logo.png" alt="">
+                </div>
+                <div class="footer-col-2">
+                    <a href="ToS.php">
+                        <p>ToS (Term of Service)</p>
+                    </a>
+
+                </div>
+                <div class="footer-col-3">
+                    <a href="PrivacyPolicy.php">
+                        <p>Privacy Policy</p>
+                    </a>
+
+                </div>
+
+                <div class="footer-col-4">
+                    <a href="CopyRight.php">
+                        <p>Copy right 2021</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
