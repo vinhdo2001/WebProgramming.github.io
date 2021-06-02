3faedcf4b2295716d7d79001a5e7728919d8061b
NguyenDangLamPhuong <s3754105@rmit.edu.vn>
2021-06-02 19:21:31 +0700

    phuong edit

--- phpForHomePage/store_functions.php
+++ phpForHomePage/store_functions.php
@@ -1,16 +1,16 @@
 <?php
-require 'phpForHomePage/product_Functions.php';
-function readAllStore() {
-  //This function read all the store lines by lines
-  $fileName = '../csvFile/stores.csv';
-  $open = fopen($fileName, 'r');
-  $first = fgetcsv($open);
+require 'product_functions.php';
+
+function read_all_stores() {
+  $file_name = '../csvFile/stores.csv';
+  $fp = fopen($file_name, 'r');
+  $first = fgetcsv($fp);
   $stores = [];
-  while ($row = fgetcsv($open)) {
+  while ($row = fgetcsv($fp)) {
     $i = 0;
     $store = [];
-    foreach ($first as $colName) {
-      $store[$colName] =  $row[$i];
+    foreach ($first as $col_name) {
+      $store[$col_name] =  $row[$i];
       $i++;
     }
     $stores[] = $store;
@@ -45,8 +45,8 @@ function read_newest_stores() {
   while ($row = fgetcsv($fp)) {
     $i = 0;
     $store = [];
-    foreach ($first as $colName) {
-      $store[$colName] =  $row[$i];
+    foreach ($first as $col_name) {
+      $store[$col_name] =  $row[$i];
       $i++;
     }
     $stores[] = $store;
@@ -55,17 +55,16 @@ function read_newest_stores() {
   return $stores;
 
 }
-function readFeaturedStores() {
-  //This function read all the featured stores lines by lines
-  $fileName = '../csvFile/stores.csv';
-  $open = fopen($fileName, 'r');
-  $first = fgetcsv($open);
+function read_featured_stores() {
+  $file_name = '../csvFile/stores.csv';
+  $fp = fopen($file_name, 'r');
+  $first = fgetcsv($fp);
   $stores = [];
   while ($row = fgetcsv($fp)) {
     $i = 0;
     $store = [];
-    foreach ($first as $colName) {
-      $store[$colName] =  $row[$i];
+    foreach ($first as $col_name) {
+      $store[$col_name] =  $row[$i];
       $i++;
     }
     if ($store['featured']=="TRUE"){
@@ -76,11 +75,10 @@ function readFeaturedStores() {
 }
 
 
-function getStore($storeID) {
-  //Get the a store ID for testing
-  $stores = readAllStore();
+function get_store($store_id) {
+  $stores = read_all_stores();
   foreach ($stores as $p) {
-    if ($p['id'] == $storeID) {
+    if ($p['id'] == $store_id) {
       return $p;
     }
   }
